89dcfa215cab0f2d79417eda6c4ad13c879f0cbb
Divyansh <maddhesiyadivyansh@gmail.com>
2024-01-19 20:40:59 +0530

    Added auth and database

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,13 +4,49 @@ import { useScrollTop } from "@/hooks/use-scroll-top"
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
 
 export const Navbar = () => {
+    const { isAuthenticated, isLoading} = useConvexAuth();
     const scrolled = useScrollTop();
     return (
         <div className= {cn("z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6", scrolled && "border-b shadow-sm")}>
             <Logo/>
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <p><Spinner/></p>
+                )}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button variant="ghost" size="sm">
+                                Log in
+                            </Button>
+                        </SignInButton>
+                        <SignInButton mode="modal">
+                            <Button size="sm">
+                                Get ToDo App free
+                            </Button>
+                        </SignInButton>
+                    </>
+                )}
+                {isAuthenticated && !isLoading && (
+                  <>
+                    <Button variant= "ghost" size="sm">
+                        <Link href="/documents">
+                            Enter ToDo App
+                        </Link>
+                    </Button>
+                    <UserButton
+                      afterSignOutUrl="/"  
+                    />
+                  </>  
+                )}
                 <ModeToggle/>
             </div>
         </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://upright-squid-55.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
