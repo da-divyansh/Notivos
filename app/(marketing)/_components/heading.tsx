7d7b9b0b1faab0397a8d79900370580ed114511e
Divyansh <maddhesiyadivyansh@gmail.com>
2024-01-22 19:29:28 +0530

    Added signin and signup

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,23 +1,46 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+import { SignInButton } from "@clerk/clerk-react";
 
 export const Heading = () => {
+    const { isAuthenticated, isLoading} = useConvexAuth();
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
                 Your Ideas, Documents, & Plans Unified. Welcome to
                 <span className="underline">ToDo</span>
             </h1>
-            <h3>
+            <h3 className="text-base sm:text-xl md:text-2xl font-medium">
                ToDo App is the connected workspace where <br/>
                 better, faster works happens. 
             </h3>
-            <Button>
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size="lg" />
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+            <Button asChild>
+                <Link href= "/documents">
                 Enter ToDoApp 
                 <ArrowRight className="h-4 w-4 ml-2"/>
+                </Link>
             </Button>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get TodoApp Free
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Button>
+                </SignInButton>
+            )}
         </div>
     )
 }
