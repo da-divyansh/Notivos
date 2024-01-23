bf2bd1ae9bdce2183d28390a869169ae4f2f0595
Divyansh <maddhesiyadivyansh@gmail.com>
2024-01-23 19:52:31 +0530

    Added some colors

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,7 +5,7 @@ import { Footer } from "./_components/footer";
 const MarketingPage = () => {
     return (
         <div className="min-h-full flex flex-col">
-            <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+            <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10 dark:bg-[#1F1F1F]">
                 <Heading />
                 <Heroes/>
             </div> 
