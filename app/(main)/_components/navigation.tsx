28b4896c15ba0685cfa2be5d89084ea6b6e2f966
Divyansh <maddhesiyadivyansh@gmail.com>
2024-01-24 18:23:41 +0530

    Added effect to sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,9 +1,10 @@
 "use client"
 
 import { cn } from "@/lib/utils";
+
 import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
-import { ElementRef, useRef, useState } from "react";
+import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 
 export const Navigation = () => {
@@ -16,6 +17,19 @@ export const Navigation = () => {
     const [isResetting, setIsResetting] = useState(false);
     const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
+    useEffect(() => {
+        if(isMobile){
+            collapse();
+        } else {
+            resetWidth();
+        }
+    }, [isMobile]);
+
+    useEffect(() => {
+        if(isMobile) {
+            collapse();
+        }
+    }, [pathname, isMobile])
     const handleMouseDown = (
         event: React.MouseEvent<HTMLDivElement, MouseEvent>
     ) => {
@@ -58,6 +72,18 @@ export const Navigation = () => {
 
             setTimeout(() => setIsResetting(false), 300);
         }
+    };
+
+    const collapse = () => {
+        if (sidebarRef.current && navbarRef.current) {
+            setIsCollapsed(true);
+            setIsResetting(true);
+
+            sidebarRef.current.style.width = "0";
+            navbarRef.current.style.setProperty("width", "100%");
+            navbarRef.current.style.setProperty("left", "0");
+            setTimeout(() => setIsResetting(false), 300);
+        }
     }
 
     return (
@@ -70,6 +96,7 @@ export const Navigation = () => {
             isMobile && "w-0"
             )}>
             <div
+                onClick={collapse}
                 role="button"
                 className={cn(
                     "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
@@ -98,7 +125,7 @@ export const Navigation = () => {
             )}
         >
             <nav className="bg-transparent px-3 py-2 w-full">
-                {isCollapsed && <MenuIcon role="button" className="h-6 w-6 text-muted-foreground"/>}
+                {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground"/>}
             </nav>
         </div>
         </>
