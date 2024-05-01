145a29b16728e8e8026eaa8a0cfcc8574d1112c7
Divyansh <maddhesiyadivyansh@gmail.com>
2024-05-01 21:03:32 +0530

    Added Trash button  functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -25,6 +25,7 @@ import {
     PopoverTrigger,
     PopoverContent,
 } from "@/components/ui/popover";
+import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
     const pathname = usePathname();
@@ -169,7 +170,7 @@ export const Navigation = () => {
                         className="p-0 w-72"
                         side={isMobile ? "bottom" : "right"}
                     >
-
+                        <TrashBox/>
                     </PopoverContent>
                 </Popover>
             </div>
--- convex/documents.ts
+++ convex/documents.ts
@@ -2,6 +2,7 @@ import { v } from "convex/values";
 
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
+import { Recursive } from "next/font/google";
 
 
 export const archive = mutation({
@@ -108,4 +109,119 @@ export const create = mutation({
 
         return document;
     }
-})
+});
+
+export const getTrash = query({
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+
+        const userId = identity.subject;
+        
+        const documents = await ctx.db
+            .query("documents")
+            .withIndex("by_user", (q) => 
+               q
+                  .eq("userId", userId)
+            )
+            .filter((q) => 
+              q.eq(q.field("isArchived"), true)
+            )
+            .order("desc")
+            .collect();
+
+        return documents;
+    }
+});
+
+export const restore = mutation({
+    args: {
+        id: v.id("documents")
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+
+        const userId = identity.subject;
+
+        const existingdocument = await ctx.db.get(args.id);
+
+        if(!existingdocument) {
+            throw new Error("Not Found")
+        }
+
+        if(existingdocument.userId !== userId) {
+            throw new Error("Unauthorized");
+        }
+
+        const recursiveRestore = async (documentId: Id<"documents">) => {
+            const children = await ctx.db
+                .query("documents")
+                .withIndex("by_user_parent", (q) => (
+                    q
+                        .eq("userId", userId)
+                        .eq("parentDocument", documentId)
+                ))
+                .collect();
+
+                for (const child of children) {
+                    await ctx.db.patch(child._id, {
+                        isArchived: false,
+                    });
+
+                    await recursiveRestore(child._id);
+                }
+        }
+
+        const options : Partial<Doc<"documents">> = {
+            isArchived: false,
+        };    
+
+        if(existingdocument.parentDocument) {
+            const parent = await ctx.db.get(existingdocument.parentDocument);
+            if(parent?.isArchived) {
+                options.parentDocument = undefined;
+            }
+        }
+        const document = await ctx.db.patch(args.id, options);
+
+        recursiveRestore(args.id);
+
+        return document;
+    }
+});
+
+export const remove = mutation({
+    args: {
+        id: v.id("documents")
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+
+        const userId = identity.subject;
+
+        const existingDocument = await ctx.db.get(args.id);
+
+        if (!existingDocument) {
+            throw new Error("Not Found")
+        }
+
+        if (existingDocument.userId !== userId) {
+            throw new Error("Unauthorized");
+        }
+
+        const document = await ctx.db.delete(args.id);
+
+        return document;
+    }
+}); 
